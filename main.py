212876bc49e5ac9e9f5bb29d1e6712c34de49276
MCJAMES001 <mcnyangweso@gmail.com>
2025-12-15 17:47:16 +0100

    all Na's Dropped

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
 
-Weather = pd.read_csv("Weather_data.csv")
+
+Weather = pd.read_csv("Weather_Data.csv")
 print(Weather.head())
 
 print(Weather.describe())
 
 print(Weather.info())
+
+Weather.dropna(inplace=True)
+
+print(Weather.head())
